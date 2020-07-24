6f6cf5bb8f44c3c3c20a01c38446c489e01da157
Xeltica <xeltica@gmail.com>
2020-07-24 23:08:43 +0900

    mfm support

--- src/render.ts
+++ src/render.ts
@@ -1,11 +1,20 @@
-import config from './config';
 import views from 'koa-views';
+import { parse, toHtml } from 'mfmf';
+import { parsePlain } from 'mfmf/dist/script/mfm/parse';
+
+
+import config from './config';
 import { User } from './models/User';
 import { Note } from './models/Note';
 
 export const render = views(__dirname + '/views', {
 	extension: 'pug', options: {
 		...config,
+		mfmToHtml: (text: string, plain = false) => {
+			return toHtml(plain ? parsePlain(text) : parse(text), [], {
+				url: '',
+			});
+		},
 		getAcct: (user: User) =>  user.host ? `@${user.username}@${user.host}` : `@${user.username}`,
 		getUserName: (user: User) => user.name || user.username,
 		getVisibility: (note: Note) => {
